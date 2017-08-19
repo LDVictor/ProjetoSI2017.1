637af3740e318199e68eec34269a5b116929ef64
LDVictor <victor.borges@ccc.ufcg.edu.br>
2017-08-19 10:09:10 -0300

    Diretório alterado para funcionar no Heroku.

---
+++ src/main/java/com/ufcg/si1/service/interfaces/QueixaService.java
@@ -0,0 +1,26 @@
+package com.ufcg.si1.service.interfaces;
+
+import java.util.Collection;
+
+import com.ufcg.si1.model.Queixa;
+import com.ufcg.si1.model.enums.SituacaoGeralQueixas;
+
+public interface QueixaService {
+
+	Collection<Queixa> getQueixas();
+
+	Queixa procuraQueixaId(Long id);
+
+	boolean deletaQueixaId(Long id);
+
+	SituacaoGeralQueixas consultaSituacaoGeral();
+
+	Queixa addQueixa(Queixa queixa);
+
+	boolean atualizaQueixa(Queixa queixa);
+
+	public void deletaTodasQueixas();
+	
+	public long quantidadeQueixasAbertas();
+
+}
