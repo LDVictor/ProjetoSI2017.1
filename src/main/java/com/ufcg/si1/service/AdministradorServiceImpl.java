6ab6bcb1c9d0410be690895a5da04ac67090d0fa
LDVictor <victor.borges@ccc.ufcg.edu.br>
2017-08-24 15:15:11 -0300

    Interface de AdministradorService criada. Login do admin concluído!

--- src/main/java/com/ufcg/si1/service/AdministradorServiceImpl.java
+++ src/main/java/com/ufcg/si1/service/AdministradorServiceImpl.java
@@ -3,24 +3,26 @@ package com.ufcg.si1.service;
 import com.ufcg.si1.model.Administrador;
 import com.ufcg.si1.repositorios.AdministradorRepositorio;
 
+import com.ufcg.si1.service.interfaces.AdministradorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("AdministradorServiceImpl")
-public class AdministradorServiceImpl {
+public class AdministradorServiceImpl implements AdministradorService {
     @Autowired
     private AdministradorRepositorio adminRepositorio;
 
+    @Override
     public Administrador logar(Administrador admin) {
         return logar(admin.getLogin(), admin.getSenha());
     }
 
-    public Administrador logar(String login, String senha) {
+    private Administrador logar(String login, String senha) {
         List<Administrador> admins = adminRepositorio.findAll();
         for (Administrador admin : admins) {
-            if (admin.getLogin().equals(login)) {
+            if (admin.getLogin().equalsIgnoreCase(login)) {
                 if(admin.getSenha().equals(senha)) {
                     return admin;
                 }
---
+++ src/main/java/com/ufcg/si1/service/interfaces/AdministradorService.java
@@ -0,0 +1,8 @@
+package com.ufcg.si1.service.interfaces;
+
+import com.ufcg.si1.model.Administrador;
+
+public interface AdministradorService {
+
+    public Administrador logar(Administrador administrador);
+}
