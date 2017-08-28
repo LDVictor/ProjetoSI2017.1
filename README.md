c5f7be452b108679542b52d1af9f0e807be1ac1c
LDVictor <victor.borges@ccc.ufcg.edu.br>
2017-08-28 11:59:11 -0300

    Agora sim, finalizado.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# disqueSaude-si1-backend
+# Disque Saúde - Instrução para Executar o Projeto
+
+1 - Vá em src/main/resources e abra o arquivo application.properties
+
+2 - Edite o campo "spring.datasource.password" para a senha de seu postgre
+
+3 - Execute o Spring (classe DisqueSaudeApp) em seu programa de preferência (Eclipse, IntelliJ, etc.)]
+
+4 - Acesse o link: http://disque-saude.herokuapp.com/
--- app/core/controller/unidadeSaudeController.js
+++ app/core/controller/unidadeSaudeController.js
@@ -1,6 +1,6 @@
 app.controller("adicionaPostoSaude", function ($scope, $http, toastr, $location) {
     $scope.addUnidadeSaude = function (postoSaude) {
-        $http.post("/DisqueSaudeApp/api/unidade/", JSON.stringify(postoSaude)).then(function success(response) {
+        $http.post("http://localhost:5000/DisqueSaudeApp/api/unidade/", JSON.stringify(postoSaude)).then(function success(response) {
             toastr.success("Unidade Saude adicionada com sucesso!");
             $location.path('/createdunidadesaude/' + response.data.id);
         }, function error(error) {
@@ -12,7 +12,7 @@ app.controller("adicionaPostoSaude", function ($scope, $http, toastr, $location)
 app.controller("adicionaHospital", function ($scope, $http, toastr, $location) {
 
     $scope.addUnidadeSaude = function (hospital) {
-        $http.post("/DisqueSaudeApp/api/unidade/hospital",
+        $http.post("http://localhost:5000/DisqueSaudeApp/api/unidade/hospital",
             JSON.stringify(hospital)).then(function success(response) {
             toastr.success("Unidade Saude adicionada com sucesso!");
             $location.path('/createdunidadesaude/' + response.data.id);
@@ -29,7 +29,7 @@ app.controller("procuraMediaMedicoPaciente", function ($scope, $http) {
 
     $scope.searchAveragePerPatient = function (id) {
         $http.get(
-            "/DisqueSaudeApp/api/geral/medicos/" + id)
+            "http://localhost:5000/DisqueSaudeApp/api/geral/medicos/" + id)
             .then(function successCallback(response) {
                 $scope.average = response.data;
             }, function errorCallback(error) {
@@ -45,7 +45,7 @@ app.controller("procuraUnidadeSaude", function ($scope, $http) {
 
     $scope.searchHU = function (neighborhood) {
         $http.get(
-            "/DisqueSaudeApp/api/unidade/"
+            "http://localhost:5000/DisqueSaudeApp/api/unidade/"
             + neighborhood).then(function success(response) {
             $scope.units = response.data;
             console.log("Foram encontradas Unidades de saúde");
@@ -68,7 +68,7 @@ app.controller("mensagemCriacaoUnidadeSaude", function ($scope, $routeParams) {
 app.controller("retornaUnidadeSaudeCtrl", function ($scope, $http) {
     $scope.listaUnidadeSaude = [];
     $scope.retornaUnidadeSaude = function () {
-        $http.get("/DisqueSaudeApp/api/unidade/listartodasus").then(function sucessCallback(response) {
+        $http.get("http://localhost:5000/DisqueSaudeApp/api/unidade/listartodasus").then(function sucessCallback(response) {
             $scope.listaUnidadeSaude = response.data;
         }, function errorCallback(error) {
             alert("Unidades Não Encontrada");
